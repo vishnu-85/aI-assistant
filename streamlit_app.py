8dbdbe81c7374599994dbdc3680acfbd7538688f
Vishnu Kant <30721023+vishnu-85@users.noreply.github.com>
2026-04-01 09:41:41 +0000

    table data set

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
+import pandas as pd
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+confusion_matrix = pd.DataFrame(
+    {
+        "Predicted Cat": [85, 3, 2, 1],
+        "Predicted Dog": [2, 78, 4, 0],
+        "Predicted Bird": [1, 5, 72, 3],
+        "Predicted Fish": [0, 2, 1, 89],
+    },
+    index=["Actual Cat", "Actual Dog", "Actual Bird", "Actual Fish"],
 )
+st.table(confusion_matrix)
